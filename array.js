c2adc969b67ee8526a6c35ef7a05ae3f7411e3cb
ANDRE AUGUSTO CESAR QUEIROZ DE SOUZA <andresouza6178@digitalcollege.internal>
2025-06-10 20:18:40 -0300

    array.js objeto.js

---
+++ array.js
@@ -0,0 +1,75 @@
+// Lista de itens do comentários:
+//1. Criar
+let nomes = ["Ana","Bia","Carlos", "Daniel","Eduardo"];
+let carros = []
+
+let idades = [12,'25','24']
+
+
+const  cidadesArgentina = ['Buenos Aires','xique xique bahia']
+
+
+
+
+
+
+
+// length: retorna o numero com a quantidade de itens da lista
+//console.log(`Tamanho: ${cidadesArgentina.length}`)
+
+
+
+
+
+
+
+
+// 2.Limpar
+console.log(idades)
+
+idades =[]
+
+
+
+
+
+//3. adicionar elemento
+
+carros.push("Fusca", 3);
+console.log(carros);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//4. remover elementos
+//pop: remove o ultimo elemento
+//shift: remove o primeiro elemento
+/*onsole.log(nomes);
+nomes.pop();
+console.log(nomes);
+nomes.shift
+console.log(nomes);
+console.log(nomes[1])
+console.log(`${nomes[1]} ${nomes[2]}`);
+//let nomeProprio =`${nomes[1]} ${nomes[2]}´ */
+
+
+
+
+
+//6. percorrer os itens
+
+let frutas = ['acabxi', 'goiaba', 'manga', 'maracujá','kiwi'];
+frutas.forEach(function(fruta){
+    console.log(fruta);
+
+})
